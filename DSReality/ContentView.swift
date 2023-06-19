9944c8ca419442ccfc86854cb40a1adbfe1a2be5
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2023-06-19 16:47:36 -0400

    Initial Commit

---
+++ DSReality/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  DSReality
+//
+//  Created by Zhuowei Zhang on 2023-06-19.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif

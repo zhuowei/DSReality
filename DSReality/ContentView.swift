177db1e94645256438d8f8a7656e1552a0c442a2
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2023-06-19 19:35:25 -0400

    Run melonDS using DeltaCore

--- DSReality/ContentView.swift
+++ DSReality/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import RealityKit
+import MelonDSDeltaCore
+import DeltaCore
 
 struct ContentView : View {
     var body: some View {
@@ -17,14 +19,20 @@ struct ContentView : View {
 struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
-        
+              
         let arView = ARView(frame: .zero)
+      
+      Delta.register(MelonDS.core)
+      let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+      let game = Game(fileURL: documentDirectory.appendingPathComponent("rom.bin"), type: .ds)
+      let emulatorCore = EmulatorCore(game: game)
+      emulatorCore?.start()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        //let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        //arView.scene.anchors.append(boxAnchor)
         
         return arView
         
--- Podfile
+++ Podfile
@@ -3,10 +3,10 @@
 
 target 'DSReality' do
   # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
+  # use_frameworks!
 
   # Pods for DSReality
-  pod 'DeltaCore', :git => 'https://github.com/rileytestut/DeltaCore', :commit => 'd320d817495436dfa3495ae8df5b3a1fe0e304b7'
+  pod 'DeltaCore', :path => '/Volumes/thickhd/docs/repos/DeltaCore'
   pod 'MelonDSDeltaCore', :path => '/Volumes/thickhd/docs/repos/MelonDSDeltaCore'
 
 end

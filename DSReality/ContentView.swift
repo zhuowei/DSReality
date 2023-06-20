fba06c810ef318a57562b3b2a4e432bfc1eec8b6
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2023-06-19 20:46:40 -0400

    enough to boot a game

--- DSReality/ContentView.swift
+++ DSReality/ContentView.swift
@@ -10,9 +10,26 @@ import RealityKit
 import MelonDSDeltaCore
 import DeltaCore
 
+var emulatorCore: EmulatorCore!
+
+func getOrStartEmulator() -> EmulatorCore {
+  if let emulatorCore = emulatorCore {
+    return emulatorCore
+  }
+  Delta.register(MelonDS.core)
+  let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+  let game = Game(fileURL: documentDirectory.appendingPathComponent("mario_kart_ds.nds"), type: .ds)
+  emulatorCore = EmulatorCore(game: game)
+  emulatorCore.start()
+  return emulatorCore
+}
+
 struct ContentView : View {
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+      ZStack {
+        //ARViewContainer().edgesIgnoringSafeArea(.all)
+        GameViewContainer()
+      }
     }
 }
 
@@ -21,12 +38,6 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
               
         let arView = ARView(frame: .zero)
-      
-      Delta.register(MelonDS.core)
-      let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-      let game = Game(fileURL: documentDirectory.appendingPathComponent("rom.bin"), type: .ds)
-      let emulatorCore = EmulatorCore(game: game)
-      emulatorCore?.start()
         
         // Load the "Box" scene from the "Experience" Reality File
         //let boxAnchor = try! Experience.loadBox()
@@ -40,6 +51,16 @@ struct ARViewContainer: UIViewRepresentable {
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct GameViewContainer: UIViewRepresentable {
+  func makeUIView(context: Context) -> GameView {
+    let gameView = GameView(frame: .zero)
+    getOrStartEmulator().add(gameView)
+    return gameView
+  }
+  func updateUIView(_ uiView: GameView, context: Context) {
+  }
 }
 
 #if DEBUG
